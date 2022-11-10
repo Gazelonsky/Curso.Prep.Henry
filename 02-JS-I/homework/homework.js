8a8d32ff05ec20f39df52e4f87dab2e171f3d923
Gazelonsky <gazelonsky@gmail.com>
2022-11-10 13:25:10 -0600

    PARES E IMPARES

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -85,34 +85,47 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num < 90){
+    return true;
+  }
+  return false;
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num > 50){
+    return true;
+  }
+  return false;
 }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  
+  var c = x % y
+  return c
 }
 
 function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if ((num % 2) == 0) {
+    return true;    
+  }
+  return false;
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (((num + 1) % 2)  > 0) {
+    return false;
+  }
+  return true;
 }
 
 function elevarAlCuadrado(num) {
